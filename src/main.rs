c96215d746965a074b8c1fc91ef064e3c31880ed
David Björklund <davbjor@kth.se>
2024-09-13 16:56:40 +0200

    Working version

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,21 @@ impl Board {
     pub fn place_bricka(&mut self, movi: i64) -> bool {
 		let mut was_valid: bool = false;
 		for i in 0..6 {
-			let x = movi+i*6;
+			let x = movi+i*7;
 			if self.board[x as usize] == 'O' || self.board[x as usize] == 'X' {
 				continue;
 			}
-			if self.turn == false {self.board[x as usize] = 'O'}
-			else {self.board[x as usize] = 'X'}
+			if self.turn == false {self.board[x as usize] = 'O';}
+			else {self.board[x as usize] = 'X';}
 			was_valid = true;
+            break;
 		}
 		if !was_valid {
 			println!("Invalid Move!");
 			return false;
 		}
+
+
         if self.turn {
             self.turn = false;
         }
@@ -48,21 +51,21 @@ impl Board {
 				let right = x+3;
 				let mut has_winner: bool = false;
 				
-				let diagl1 = (x-1)+(y+1)*6;
-				let diagl2 = (x-2)+(y+2)*6;
-				let diagl3 = (x-3)+(y+3)*6;
+				let diagl1 = (x-1)+(y+1)*7;
+				let diagl2 = (x-2)+(y+2)*7;
+				let diagl3 = (x-3)+(y+3)*7;
 				
 				let left1 = i-1;
 				let left2 = i-2;
 				let left3 = i-3;
 
-				let up1 = i+6;
-				let up2 = i+12;
-				let up3 = i+18;
+				let up1 = i+7;
+				let up2 = i+14;
+				let up3 = i+21;
 
-				let diagr1 = (x+1)+(y+1)*6;
-				let diagr2 = (x+2)+(y+2)*6;
-				let diagr3 = (x+3)+(y+3)*6;
+				let diagr1 = (x+1)+(y+1)*7;
+				let diagr2 = (x+2)+(y+2)*7;
+				let diagr3 = (x+3)+(y+3)*7;
 
 	
 				if (left >= 0 && up < 7 && self.board[i as usize] == self.board[diagl1 as usize] && self.board[i as usize] == self.board[diagl2 as usize] && self.board[i as usize] == self.board[diagl3 as usize]) || (left >= 0 && self.board[i as usize] == self.board[left1 as usize] && self.board[i as usize] == self.board[left2 as usize] && self.board[i as usize] == self.board[left3 as usize]) || (up < 7 && self.board[i as usize] == self.board[up1 as usize] && self.board[i as usize] == self.board[up2 as usize] && self.board[i as usize] == self.board[up3 as usize]) || (right < 7 && up < 7 && self.board[i as usize] == self.board[diagr1 as usize] && self.board[i as usize] == self.board[diagr2 as usize] && self.board[i as usize] == self.board[diagr3 as usize]){has_winner = true;}
@@ -84,10 +87,10 @@ impl Board {
             for x in 0..7 {
                 //print!("{}", self.board[y*7+x]);
                 if self.board[y*7+x] == 'X' {
-                    print!("{}", (self.board[y*7+x]).to_string().red());
+                    print!("{}", (self.board[y*7+x]).to_string().yellow());
                 }
                 else if self.board[y*7+x] == 'O' {
-                    print!("{}", (self.board[y*7+x]).to_string().yellow());
+                    print!("{}", (self.board[y*7+x]).to_string().red());
                 }
                 else {
                     print!("{}", self.board[y*7+x]);
@@ -107,7 +110,7 @@ impl Board {
 fn main() {
     
     //let start: [char, 42] = ['.'; 42];
-    let board = Board { board: ['.'; 42], turn: true };
+    let mut board = Board { board: ['.'; 42], turn: true };
     
     board.print_board();
 
@@ -136,11 +139,12 @@ fn main() {
             continue;
         }
 
-        /*
-        if board.place_bricka(num-1) == false {
+        
+        let send = num - 1;
+        if board.place_bricka(send) == false {
             println!("Could not place bricka at {}!\n, try again", num);
         }
-        */
+        
         board.print_board();
         
         let c: char = board.has_won();
@@ -156,6 +160,7 @@ fn main() {
                 s = "Red".to_string();
             };
             println!("{} has won", s);
+            break;
         }
 
     }
